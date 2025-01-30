749b30bf160c47563f5876caae508bcc2a4f6175
furamon <furamon913@gmail.com>
2025-01-30 17:38:26 +0900

    ライブラリ更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,44 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://magialaboratory.com/',
-    markdown: {
-        remarkPlugins: [remarkReadingTime],
-        drafts: true,
-        shikiConfig: {
-            theme: 'material-theme-palenight',
-            wrap: true
-        }
-    },
-    integrations: [mdx({
-        syntaxHighlight: 'shiki',
-        shikiConfig: {
-            experimentalThemes: {
-                light: 'vitesse-light',
-                dark: 'material-theme-palenight'
-            },
-            wrap: true
-        },
-        drafts: true
-		}), sitemap(), tailwind()],
-    image: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'assets.tina.io'
-            }
-        ]
-    }
+	site: 'https://magialaboratory.com/',
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		}
+	},
+	integrations: [
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				experimentalThemes: {
+					light: 'vitesse-light',
+					dark: 'material-theme-palenight'
+				},
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap(),
+		tailwindcss()
+	],
+	image: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'assets.tina.io'
+			}
+		]
+	},
+	vite: {
+		plugins: [tailwindcss()]
+	}
 })
